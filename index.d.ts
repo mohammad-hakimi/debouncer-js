adb9c2990884fd57188e897ae5451a756afdd24f
mohammad-hakimi <hakimi.mohammad.n@gmail.com>
2022-08-21 11:14:36 +0430

    Version 1.0.1 release - Declare module

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,16 @@
-declare class Debouncer {
-    constructor();
+declare module 'debouncer-js' {
+    class Debouncer {
+        constructor();
 
-    /**
-     * Creates a delayed callback function call and postpone it on each call repeat until the delay duration ends.
-     * @param {function(...args: any[]): void} callback A named function should be passed. Anonymous functions and arrow functions may not
-     * work properly.
-     * @param {number} delay The delay to postpone the calling callback function in milliseconds.
-     * @returns {function(...args: any[]): void} Debounced function derived from the passed callback.
-     */
-    debounce(callback: (...args: any[]) => void, delay: number);
-    clear(callback: ((...args: any[]) => void) | string);
-    flush();
+        /**
+         * Creates a delayed callback function call and postpone it on each call repeat until the delay duration ends.
+         * @param {function(...args: any[]): void} callback A named function should be passed. Anonymous functions and arrow functions may not
+         * work properly.
+         * @param {number} delay The delay to postpone the calling callback function in milliseconds.
+         * @returns {function(...args: any[]): void} Debounced function derived from the passed callback.
+         */
+        debounce(callback: (...args: any[]) => void, delay: number);
+        clear(callback: ((...args: any[]) => void) | string);
+        flush();
+    }
 }
