f36e09679f92f3ddec87260827c2a8f8fdf5c67f
mohammad-hakimi <hakimi.mohammad.n@gmail.com>
2022-08-21 11:12:20 +0430

    Version 1.0.0 release

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare class Debouncer {
+    constructor();
+
+    /**
+     * Creates a delayed callback function call and postpone it on each call repeat until the delay duration ends.
+     * @param {function(...args: any[]): void} callback A named function should be passed. Anonymous functions and arrow functions may not
+     * work properly.
+     * @param {number} delay The delay to postpone the calling callback function in milliseconds.
+     * @returns {function(...args: any[]): void} Debounced function derived from the passed callback.
+     */
+    debounce(callback: (...args: any[]) => void, delay: number);
+    clear(callback: ((...args: any[]) => void) | string);
+    flush();
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+class Debouncer {
+    constructor() {
+        this.timeoutList = {}
+    }
+
+    /**
+     * Creates a delayed callback function call and postpone it on each call repeat until the delay duration ends.
+     * @param {function(...args: any[]): void} callback A named function should be passed. Anonymous functions and arrow functions may not
+     * work properly.
+     * @param {number} delay The delay to postpone the calling callback function in milliseconds.
+     * @returns {function(...args: any[]): void} Debounced function derived from the passed callback.
+     */
+    debounce(callback, delay) {
+        return (...args) => {
+            if (this.timeoutList[callback.name]) {
+                clearTimeout(this.timeoutList[callback.name])
+            }
+            this.timeoutList[callback.name] = setTimeout(callback, delay, args ?? [])
+        }
+    }
+
+
+    clear(callback) {
+        const foundTimeout = this.timeoutList[typeof callback === 'function' ? callback.name : callback]
+        clearTimeout(foundTimeout)
+        foundTimeout && delete this.timeoutList[typeof callback === 'function' ? callback.name : callback]
+    }
+
+    flush() {
+        Object.keys(this.timeoutList).forEach(key => {
+            clearTimeout(this.timeoutList[key])
+            delete this.timeoutList[key]
+        })
+    }
+}
+
+export default Debouncer
